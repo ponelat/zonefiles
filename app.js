457ff7607172280ca1762db5831a620ea9374b76
Josh Ponelat <josh@ponelat.com>
2015-11-28 19:35:52 +0200

    add conditionals in app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 var zi = require('./zone-importer')()
+var config = require('./config')
 zi.setupDB(function() {
-  zi.runMulti('x*', {delAfter: true}, zi.db.close.bind(zi))
+  if(config.ZONEFILES) {
+    zi.runMulti(config.ZONEFILES, {delAfter: true}, zi.db.close.bind(zi))
+    return
+  } else if(config.ZONEFILE) {
+    zi.run(config.ZONEFILE, zi.db.close.bind(zi))
+    return
+  } else {
+    console.log('Not Running, ZONEFILES? empty')
+    console.log('config', config)
+  }
 })
